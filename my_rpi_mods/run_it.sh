69d0b6cfbabbabbe049f78ea5c40dc59e0fea777
Paul Bouchier <paul.bouchier@gmail.com>
2024-01-05 21:48:53 -0600

    Dockerfile cleanup and add my_rpi_mods

---
+++ my_rpi_mods/run_it.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script uses Docker to launch a container in which to run ROS
+
+set -u # Treat empty variables as an error.
+
+# Some useful values
+user="bouchier"
+home="/home/$user"
+RM="--rm"
+
+# support the -keep argument to not remove the container upon exit
+if [ $# -ne 0 ]; then
+  if [ $1 == "-keep" ]; then
+    RM=""; shift 1; echo "not removing container"
+  else
+    echo "Usage: $0 [-keep]"; exit
+  fi
+fi    
+
+echo "running ${ACTIVE_DOCKER} docker with home " $home " user " $user 
+docker run                                                     \
+    --net=host                                                 \
+    -t                                                         \
+    ${RM}                                                      \
+    --privileged                                               \
+    -v $home/.bash_history:$home/.bash_history                 \
+    -v $home/ros2_ws:$home/ros2_ws                             \
+    -v $home/bag_files:$home/bag_files                         \
+    -v /tmp/.X11-unix:/tmp/.X11-unix                           \
+    -v /tmp:/tmp                                               \
+    --user $user                                               \
+    -w $home                                                   \
+    -i                                                         \
+    ${ACTIVE_DOCKER}                                           \
+    $@
+
+#    -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK):ro  \
+#    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK                            \
